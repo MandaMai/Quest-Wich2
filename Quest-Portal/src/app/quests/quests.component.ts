a63f58c54751a7891937a9f7f2a72407b0aaa9c0
MandaMai <a.maifeld@hotmail.com>
2018-05-04 22:35:00 -0700

    end of tutorial 1 and 2

---
+++ Quest-Portal/src/app/quests/quests.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Quest } from '../quest';
+import { QUESTS } from '../mock-quests';
+
+@Component({
+  selector: 'app-quests',
+  templateUrl: './quests.component.html',
+  styleUrls: ['./quests.component.scss']
+})
+export class QuestsComponent implements OnInit {
+  quests = QUESTS;
+
+  selectedQuest: Quest;
+
+  onSelect(quest: Quest): void {
+    this.selectedQuest = quest;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
